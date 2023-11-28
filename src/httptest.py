2b2017f0a28e7d9cee45bccae3e32878f912fdb0
nramac <1305289754@qq.com>
2023-11-28 20:01:18 +0800

    RestRequest

--- src/httptest.py
+++ src/httptest.py
@@ -1,21 +1,30 @@
-import requests
-
 from src.Dto.keywords import Method
+from src.restrequest import RestRequest
 
 url = "http://127.0.0.1:30080/api/v4/projects"
 verb = Method.POST
 payload = {
-    "name": "fhseuifaed",
+    "name": "dwadawd",
     "template_name": ""
+}
+body = {
+    "cadence": "daily",
+    "enabled": "true",
+    "keep_n": "string",
+    "older_than": "1",
 }
 
 headers = {
-    'Content-Type': 'applications/json',
+    'Content-Type': 'application/x-www-form-urlencoded',
     'Authorization': 'Bearer 57f1a0567beaaee4e33a17dd231b35d14eba0da866b51067480dded8dd9c899a'
 }
 
-feedback = requests.post(url=url, headers=headers, params=payload)
-# sender = RestRequest()
-# s, r = sender.send_request(Method.POST, url, headers)
-# sc, response = sender.send_request(verb=verb, url=url, headers=headers, query=payload)
-print(feedback)
+kwargs = dict()
+kwargs["query"] = payload
+kwargs["json"] = body
+
+sender = RestRequest()
+s, r = sender.send_request(Method.POST, url, headers, **kwargs)
+
+print(s)
+print(r)
--- src/restrequest.py
+++ src/restrequest.py
@@ -1,9 +1,9 @@
-import json
 from typing import Tuple, Union
 
 import requests
 from loguru import logger
 
+from rest import ContentType
 from src.Dto.keywords import Method
 
 
@@ -15,54 +15,46 @@ class RestRequest:
         if url is None or "":
             logger.warning("Url cannot be null")
             return 700, dict()
-        else:
-            self.url = url
-        self.headers = headers
-        self.query = kwargs.get("query", None)
-        self.file = kwargs.get("file", None)
         if verb == Method.POST:
-            self.body = kwargs.get("body") if kwargs.get("body") is not None else None
-            status_code, response_content = self.send_post_request()
+            status_code, response_content = self.send_post_request(url, headers, **kwargs)
         elif verb == Method.GET:
-            status_code, response_content = self.send_get_request()
+            status_code, response_content = self.send_get_request(url, headers, **kwargs)
         elif verb == Method.PUT:
-            self.body = kwargs.get("body") if kwargs.get("body") is not None else None
-            status_code, response_content = self.send_put_request()
+            status_code, response_content = self.send_put_request(url, headers, **kwargs)
         elif verb == Method.DELETE:
-            status_code, response_content = self.send_delete_request()
+            status_code, response_content = self.send_delete_request(url, headers, **kwargs)
         else:
             raise TypeError(f"Do not support the http method {verb} now")
-
         return status_code, response_content
 
     @staticmethod
-    def send_post_request(self) -> Tuple[int, Union[str, dict]]:
-        if self.body is None:
-            feedback = requests.post(url=self.url, headers=self.headers, params=self.query)
-        elif self.get_content_type() == "applications/json":
-            feedback = requests.post(url=self.url, headers=self.headers, params=self.query, json=json.dumps(self.body))
+    def send_post_request(url, headers, **kwargs) -> Tuple[int, Union[str, dict]]:
+        if headers.get("Content-Type", None) == ContentType.JSON.value:
+            feedback = requests.post(url=url, headers=headers, params=kwargs.get("query", None),
+                                     data=kwargs.get("body", None), files=kwargs.get("files", None), timeout=10)
         else:
-            feedback = requests.post(url=self.url, headers=self.headers, params=self.query, data=self.body)
+            feedback = requests.post(url=url, headers=headers, params=kwargs.get("query", None),
+                                     json=kwargs.get("body", None), files=kwargs.get("files", None), timeout=10)
         return RestRequest.get_response_info(feedback)
 
     @staticmethod
-    def send_get_request(self) -> Tuple[int, Union[str, dict]]:
-        feedback = requests.get(url=self.url, headers=self.headers, params=self.query)
+    def send_get_request(url, headers, **kwargs) -> Tuple[int, Union[str, dict]]:
+        feedback = requests.get(url=url, headers=headers, params=kwargs.get("query", None), timeout=10)
         return RestRequest.get_response_info(feedback)
 
     @staticmethod
-    def send_put_request(self) -> Tuple[int, Union[str, dict]]:
-        if self.body is None:
-            feedback = requests.post(url=self.url, headers=self.headers, params=self.query)
-        elif self.get_content_type() == "applications/json":
-            feedback = requests.post(url=self.url, headers=self.headers, params=self.query, json=json.dumps(self.body))
+    def send_put_request(url, headers, **kwargs) -> Tuple[int, Union[str, dict]]:
+        if headers.get("Content-Type", None) == ContentType.JSON.value:
+            feedback = requests.post(url=url, headers=headers, params=kwargs.get("query", None),
+                                     data=kwargs.get("body", None), files=kwargs.get("files", None), timeout=10)
         else:
-            feedback = requests.post(url=self.url, headers=self.headers, params=self.query, data=self.body)
+            feedback = requests.post(url=url, headers=headers, params=kwargs.get("query", None),
+                                     json=kwargs.get("body", None), files=kwargs.get("files", None), timeout=10)
         return RestRequest.get_response_info(feedback)
 
     @staticmethod
-    def send_delete_request(self) -> Tuple[int, Union[str, dict]]:
-        feedback = requests.delete(url=self.url, headers=self.headers, params=self.query)
+    def send_delete_request(url, headers, **kwargs) -> Tuple[int, Union[str, dict]]:
+        feedback = requests.delete(url=url, headers=headers, params=kwargs.get("query", None), timeout=10)
         return RestRequest.get_response_info(feedback)
 
     @staticmethod
@@ -73,7 +65,3 @@ class RestRequest:
         except:
             response = feedback.text
         return status_code, response
-
-    @staticmethod
-    def get_content_type(self):
-        return "applications/json" if self.headers.get("Content-Type") is None else self.headers.get("Content-Type")
